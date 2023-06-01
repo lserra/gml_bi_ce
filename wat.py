7317851a06b99534381ce3c11e3a29da0e5da23e
Laercio Serra <laercio.serra@gmail.com>
2023-05-31 22:11:37 -0300

    ENH: code refactoring

--- wat.py
+++ wat.py
@@ -14,7 +14,6 @@
 import typer
 
 import eda_tool
-from src.extract import get_file_from_gold_layer
 from src.load import put_bt_empresas_in_dw
 
 app = typer.Typer(name="gml-bi", add_completion=False, help="GML-BI Application")
@@ -24,9 +23,6 @@ app = typer.Typer(name="gml-bi", add_completion=False, help="GML-BI Application"
 def main(uf: str):
     """Starts GML-BI Application"""
     print("\n=[ GML-BI Application ]=")
-    print(f"===> Pulling data from the data lake . . .")
-    get_file_from_gold_layer.run()
-
     print(f"===> Loading data to the table in the DuckDB . . .")
     put_bt_empresas_in_dw.run()
 
