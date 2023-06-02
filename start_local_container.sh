65652e43e075656ade2b1eb7fd1966f5f23aa593
Laercio Serra <laercio.serra@gmail.com>
2023-06-02 10:50:44 -0300

    ENH: update docker container

--- start_local_container.sh
+++ start_local_container.sh
@@ -6,7 +6,8 @@
 # ==============================================================================
 echo ">>> Running BI container . . ."
 docker run --rm \
-  --network gml-cnpj \
+  -h localhost \
   --publish 8501:8501 \
+  --publish 40000:40000 \
   --name gml_cnpj_bi_ce \
   -i -t gml_cnpj/bi_ce:1.0
