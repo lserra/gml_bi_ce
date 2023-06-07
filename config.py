9522144ff3b567ebbeb2ce18511e5ed8f80d6e2e
Laercio Serra <laercio.serra@gmail.com>
2023-06-07 18:02:29 -0300

    ENH: translate to portuguese

--- config.py
+++ config.py
@@ -4,25 +4,27 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 ABOUT = """
-This is a Data Analytics Platform to get more leads, increase your sales and revenue.
-You can use those data with our other integration tools to improve your customer relationship,
-reputation tracking or get more feedback. This tool is free (Community Edition).
-There are 6M Companies (CNPJ) have been cleaned up and enriched.
-They are available, to be filtered by UF, Municipio, and Company Size.
-After that, you can export/download the data to a CSV file format and save it in your own computer.
+Esta é uma plataforma de dados que possbilita obter leads e insights.
+Também, é possível usar estes dados com nossas outras ferramentas, para melhorar o 
+relacionamento com o cliente, fazer rastreamento de reputação ou obter mais feedback.
+Esta ferramenta é gratuita (versão Community Edition). 
+Nesta base de dados, existem aproximadamente 6M de empresas (CNPJ).
+Estes dados podem ser filtrados por: UF, Município e Porte da Empresa.
+Além disso, é possível exportar os dados para um formato de arquivo CSV e salvá-los em
+seu próprio computador.
 """
 
 LGPD = """
-Os dados apresentados, são dados **NÃO SENSÍVEIS** e de **ORIGEM PÚBLICA**, e não requerem autorização
-prévia para exibição conforme
+Os dados apresentados, são dados **NÃO SENSÍVEIS** e de **ORIGEM PÚBLICA**, e não 
+requerem autorização prévia para exibição conforme
 [Decreto nº 8.777/2016](http://www.planalto.gov.br/CCIVIL_03/_Ato2015-2018/2016/Decreto/D8777.htm),
-que Institui a **Política de Dados Abertos** e
-pela [Lei nº 12.527/2011](http://www.planalto.gov.br/ccivil_03/_ato2011-2014/2011/lei/l12527.htm),
+que Institui a **Política de Dados Abertos** e pela 
+[Lei nº 12.527/2011](http://www.planalto.gov.br/ccivil_03/_ato2011-2014/2011/lei/l12527.htm),
 que regulamenta a **Garantia de acesso a Informações** previsto na Constituição Federal.
 
-Apesar da legalidade, respeitamos aqueles que expressam ativamente seu desejo em ocultar seus dados.
-Para isso utilize o link para 
-[Solicitar Privacidade](https://www.informecadastral.com.br/privacidade/era-biots-computadores-e-servicos-ltda-05111338000134).
+Apesar da legalidade, respeitamos aqueles que expressam ativamente seu desejo em ocultar 
+seus dados. Para isso utilize o link para 
+[Solicitar Privacidade](https://www.getmoreleads.com.br/privacidade/optout).
 """
 
 
